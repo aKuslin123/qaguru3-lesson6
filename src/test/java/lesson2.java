57f05a04ba5a852e49e93c7d18b9cfd2b8b9b42e
aKuslin123 <a.Kuslin123@gmail.com>
2020-11-21 21:29:28 +0700

    убрал все тесты, запихал в один, сделал по аналогии с разбором дз

--- src/test/java/lesson2.java
+++ src/test/java/lesson2.java
@@ -9,8 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class lesson2 {
     public static final String firstName = "Alexey",
@@ -23,13 +22,23 @@ public class lesson2 {
             cityName = "Delhi",
             genderMale = "Male",
             genderFemale = "Female",
-            genderOther = "Other";
+            genderOther = "Other",
+            dayOfBirth = "13",
+            monthOfBirth = "January",
+            yearOfBirth = "2000",
 
-    public static final String[] subjects = {"Arts", "Maths"},
-            hobbies = {"Sports", "Reading", "Music"};
+            subject1Prefix = "m",
+            subject1 = "Chemistry",
+            subject2Prefix = "e",
+            subject2 = "Commerce",
 
-    @BeforeAll
-    static void start() {
+            hobby1 = "Sports",
+            hobby2 = "Reading",
+            hobby3 = "Music";
+
+
+    @Test
+    void successfulFillFormTest() {
         open("https://demoqa.com/automation-practice-form");
         //firstname lastname email
         $("#firstName").val(firstName);
@@ -43,18 +52,18 @@ public class lesson2 {
         $("#userNumber").val(userNumber);
         //Date of Birth
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionContainingText("January");
-        $(".react-datepicker__year-select").selectOptionContainingText("2000");
-        $(byText("13")).click();
+        $(".react-datepicker__month-select").selectOptionContainingText(monthOfBirth);
+        $(".react-datepicker__year-select").selectOptionContainingText(yearOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
         //Subjects
-        $("#subjectsInput").val("A");
-        for (String subject : subjects) {
-            $("#subjectsInput").val(subject).pressTab();
-        }
-        //Hobbies
-        for (String hobby : hobbies) {
-            $(byText(hobby)).click();
-        }
+        $("#subjectsInput").val(subject1Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+        $("#subjectsInput").val(subject2Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+        // set hobbies
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#hobbiesWrapper").$(byText(hobby2)).click();
+        $("#hobbiesWrapper").$(byText(hobby3)).click();
         //Picture
         File file = new File(fileName);
         $("#uploadPicture").uploadFile(file);
@@ -66,79 +75,18 @@ public class lesson2 {
         $(byText("Select City")).scrollTo().click();
         $(byText(cityName)).click();
         $("#submit").scrollTo().click();
-    }
 
-    @AfterAll
-    static void end() {
-
-        $("#closeLargeModal").click();
-        $("body").shouldNotHave(text("Thanks for submitting the form"));
-    }
-
-    @Test
-    void modalFormIsOpen() {
+        // asserts
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-    }
-
-    @Test
-    void modalFormHaveStudentName() {
-        $("tr:nth-child(1)>td:nth-child(2)").shouldHave(text(firstName + " " + lastName));
-    }
-
-    @Test
-    void modalFormHaveStudentEmail() {
-        $("tr:nth-child(2)>td:nth-child(2)").shouldHave(text(userEmail));
-    }
-
-    @Test
-    void modalFormHaveGenderOther() {
-        $("tr:nth-child(3)>td:nth-child(2)").shouldHave(text(genderOther));
-    }
-
-    @Test
-    void modalFormHaveMobile() {
-        $("tr:nth-child(4)>td:nth-child(2)").shouldHave(text(userNumber));
-    }
-
-    @Test
-    void modalFormHaveDateOfBirth() {
-        String Date = $("#dateOfBirthInput").getText();
-        $("tr:nth-child(4)>td:nth-child(2)").shouldHave(text(Date));
-        System.out.println(Date);
-    }
-
-    @Test
-    void subjectsInModalDialogTest() {
-        StringBuilder sbj = new StringBuilder();
-        for (String subject : subjects) {
-            sbj.append(subject).append(", ");
-        }
-        $("tr:nth-child(6)>td:nth-child(2)").shouldHave(
-                text(sbj.delete(sbj.length() - 2, sbj.length() - 1).toString()));
-    }
-
-    @Test
-    void hobbiesInModalDialogTest() {
-        StringBuilder sbj = new StringBuilder();
-        for (String hobby : hobbies) {
-            sbj.append(hobby).append(", ");
-        }
-        $("tr:nth-child(7)>td:nth-child(2)").shouldHave(
-                text(sbj.delete(sbj.length() - 2, sbj.length() - 1).toString()));
-    }
-
-    @Test
-    void modalFormHavePicture() {
-        $("tr:nth-child(8)>td:nth-child(2)").shouldHave(text(fileName));
-    }
-
-    @Test
-    void modalFormHaveAddress() {
-        $("tr:nth-child(9)>td:nth-child(2)").shouldHave(text(currentAddress));
-    }
-
-    @Test
-    void modalFormHaveStateAndCity() {
-        $("tr:nth-child(10)>td:nth-child(2)").shouldHave(text(stateName + " " + cityName));
+        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+        $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
+        $x("//td[text()='Gender']").parent().shouldHave(text(genderOther));
+        $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+        $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
+        $x("//td[text()='Picture']").parent().shouldHave(text(fileName));
+        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+        $x("//td[text()='State and City']").parent().shouldHave(text(stateName + " " + cityName));
     }
 }
